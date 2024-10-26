d746dc5dacb8593e3e4de5e5b2810072e453092d
Fabio Milheiro <fabiomilheiro@gmail.com>
2024-10-26 12:06:22 +0100

    dynamic components

--- default/components/Example.vue
+++ default/components/Example.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 
+
+import {AnotherButton, DefaultButton} from "#components";
+
 const value = ref(1000);
 
-const onClick = ()=> {
+const onClick = () => {
   value.value++;
 };
+
+// const defaultButton = resolveComponent("DefaultButton");
+// const anotherButton = resolveComponent("AnotherButton");
+
+const useAnotherButton = computed(() => value.value % 2 == 0);
 </script>
 <template>
-  <div>Example component {{ value }} <DefaultButton @click="onClick">Increment</DefaultButton></div>
+  <div>Example component {{ value }}
+    <component :is="useAnotherButton ? AnotherButton : DefaultButton" @click="onClick">Increment</component>
+  </div>
 </template>
---
+++ default/components/Helpers/AnotherButton.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+</script>
+<template>
+  <button type="button" style="background-color:red"><slot></slot></button>
+</template>
