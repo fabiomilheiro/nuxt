b77278d6b6c2296aae171fb2c4f97d152f402bcd
Fabio Milheiro <fabiomilheiro@gmail.com>
2024-10-30 08:36:35 +0000

    style module

--- default/components/Helpers/DefaultButton.vue
+++ default/components/Helpers/DefaultButton.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 </script>
 <template>
-  <button type="button"><slot></slot></button>
+  <button type="button" :class="$style.button"><slot></slot></button>
 </template>
+<style module>
+  .button {
+    padding: 0.4rem;
+    margin: 0.1rem;
+    border-radius: 0.2rem;
+    border:1px;
+  }
+</style>
