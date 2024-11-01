ca79b8c7401b4cbeddbce28bf2231c477f0342a7
Fabio Milheiro <fabiomilheiro@gmail.com>
2024-11-01 07:48:24 +0000

    Create motion.ts

--- default/components/PiniaExampleList.vue
+++ default/components/PiniaExampleList.vue
@@ -2,9 +2,7 @@
 const listStore = useListStore();
 </script>
 <template>
-<!--  <MotionGroup  preset="fadeVisible">-->
     <ul>
       <li v-motion-roll-bottom v-for="v of listStore.list" :key="v">{{ v }}</li>
     </ul>
-<!--  </MotionGroup>-->
 </template>
